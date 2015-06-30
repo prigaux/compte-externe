33a3bc9543986d3f4c9098cb4d3559aa1d99846f
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-06-30 15:38:08 +0200

    compute max day of month (limitation: leap years not handled)

--- app/services/attrsEdit.js
+++ app/services/attrsEdit.js
@@ -5,7 +5,16 @@ angular.module('myApp')
 .service("attrsEdit", function(ws) {
     this.manager = function($scope, id, expectedStep, nextStep) {
 	$scope.label = conf.attr_labels;
+	$scope.maxDay = 31;
 	$scope.maxYear = new Date().getUTCFullYear();
+
+	var month2maxDay = [ undefined,
+			     31,29,31,30,31,30,
+			     31, // july
+			     31,30,31,30,31 ];
+	$scope.$watch('v.birthDay.month', function (month) {
+	    $scope.maxDay = month2maxDay[month] || 31;
+	});
 
 	ws.getInScope($scope, id, expectedStep);
 
