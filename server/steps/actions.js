cbcbcd0ec4ffeb820abdd4970335dc2ea5d68fe8
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-07-05 16:01:15 +0200

    handle createCompte return value (WIP) + call esup_activ_bo.setPassword if needed

--- server/steps/actions.js
+++ server/steps/actions.js
@@ -4,6 +4,7 @@ var _ = require('lodash');
 var mail = require('../mail');
 var utils = require('../utils');
 var search_ldap = require('../search_ldap');
+var esup_activ_bo = require('../esup_activ_bo');
 var conf = require('../conf');
 
 exports.getShibAttrs = function(req, _sv) {
@@ -18,11 +19,23 @@ exports.createCompte = function(req, sv) {
     var v = sv.v;
     console.log("action createCompte:", v);
     return utils.popen(JSON.stringify(v), 'createCompte', []).then(function (data) {
-
+	try { 
+	    var resp = JSON.parse(data);
+	    if (!resp.uid) console.error("createCompte should return uid");
+	    if (!resp.supannAliasLogin) console.error("createCompte should return supannAliasLogin");
+	    v.uid = resp.uid;
+	    v.supannAliasLogin = resp.supannAliasLogin;
+	} catch (e) { 
+	    throw "createCompte error:" + data;
+	}
+	    
 	if (v.supannMailPerso) {
 	    mail.sendWithTemplate('warn_user_account_created.html', { to: v.supannMailPerso, v: v });
 	}
-	return { v: v };
+	if (v.userPassword) {
+	    esup_activ_bo.setPassword(v.uid, v.userPassword);
+	}
+	return { v: v, response: {login: v.supannAliasLogin} };
     });
 };
 
