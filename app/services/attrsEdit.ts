3394b188b7ae5db917e36f9a8161088b7769230a
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-11-06 14:41:29 +0100

    transform service "attrsEdit" into a class

--- app/services/attrsEdit.ts
+++ app/services/attrsEdit.ts
@@ -1,11 +1,10 @@
-'use strict';
+class AttrsEditService {
+ constructor(private helpers, private ws, private conf) {
+ }
 
-angular.module('myApp')
-
-.service("attrsEdit", function(helpers, ws, conf) {
-    this.manager = function($scope, id, expectedStep, nextStep) {
-	$scope.label = conf.attr_labels;
-	$scope.attr_formatting = conf.attr_formatting;
+ manager($scope, id, expectedStep, nextStep) {
+	$scope.label = this.conf.attr_labels;
+	$scope.attr_formatting = this.conf.attr_formatting;
 	var accentsRange = '\u00C0-\u00FC';
 	$scope.allowedCharsInNames = "[A-Za-z" + accentsRange + "'. -]";
 	$scope.passwordPattern = /(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,}/;
@@ -16,15 +15,15 @@ angular.module('myApp')
 			     31,29,31,30,31,30,
 			     31, // july
 			     31,30,31,30,31 ];
-	$scope.$watch('v.birthDay.month', function (month) {
+	$scope.$watch('v.birthDay.month', (month) => {
 	    $scope.maxDay = month2maxDay[month] || 31;
 	});
 
-	$scope.$watch('v.homePostalAddress.postalCode', function (postalCode) {
+	$scope.$watch('v.homePostalAddress.postalCode', (postalCode) => {
 	    if (!postalCode) return;	    
 	    var address = $scope.v && $scope.v.homePostalAddress;
 	    if (address && !address.town) {
-		helpers.frenchPostalCodeToTowns(postalCode).then(function (towns) {
+		this.helpers.frenchPostalCodeToTowns(postalCode).then((towns) => {
 		    if (!address.town && towns && towns.length === 1) {
 			address.town = towns[0];
 		    }
@@ -32,19 +31,21 @@ angular.module('myApp')
 	    }
 	});
 
-	ws.getInScope($scope, id, expectedStep);
+	this.ws.getInScope($scope, id, expectedStep);
 
-	$scope.structures_search = ws.structures_search;
-	$scope.frenchPostalCodeToTowns = helpers.frenchPostalCodeToTowns;
+	$scope.structures_search = this.ws.structures_search;
+	$scope.frenchPostalCodeToTowns = this.helpers.frenchPostalCodeToTowns;
 
 	$scope.errorMessages = {};
 	
-	$scope.submit = function () {
+	$scope.submit = () => {
 	    //if (!$scope.myForm.$valid) return;
-	    ws.set(id, $scope.v).then(nextStep);
+	    this.ws.set(id, $scope.v).then(nextStep);
 	};
-	$scope.reject = function () {
-	    ws.delete(id).then(nextStep);
+	$scope.reject = () => {
+	    this.ws.delete(id).then(nextStep);
 	};
-    };
-});
+ };
+}
+
+angular.module('myApp').service("attrsEdit", AttrsEditService);
