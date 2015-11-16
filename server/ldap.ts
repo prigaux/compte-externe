171b87ae7a5e26fbf9ab92b6b433ba95ea076c87
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-11-16 17:15:10 +0100

    fix ldap.convert.datetime (or make it compatible with typescript?)

--- server/ldap.ts
+++ server/ldap.ts
@@ -184,9 +184,9 @@ export const filters = {
 export const convert = {
     from: {
         datetime: (dt: string): Date => {
-            if (!dt) return dt;
+            if (!dt) return null;
             let m = dt.match(/^(\d\d\d\d)(\d\d)(\d\d)(\d\d)(\d\d)(\d\d)Z$/);
-            return m && new Date(Date.UTC(m[1], parseInt(m[2]) - 1, m[3], m[4], m[5], m[6]));
+            return m && new Date(Date.UTC(parseInt(m[1]), parseInt(m[2]) - 1, parseInt(m[3]), parseInt(m[4]), parseInt(m[5]), parseInt(m[6])));
         },
         postalAddress: (s: string): string => (
             s && s.replace(/\$/g, "\n")
