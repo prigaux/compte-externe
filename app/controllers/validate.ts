e4a1fd7322f1185a00c60e234cb38e392578d8d5
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2017-05-12 13:18:06 +0200

    fix validate email (broken since Vuejs migration)

--- app/controllers/validate.ts
+++ app/controllers/validate.ts
@@ -3,14 +3,16 @@
 const Validate : vuejs.ComponentOption = {
   templateUrl: 'templates/validate.html',
   props: ['id'],
-  data: { finished: false },
+  data() {
+      return { finished: false, error: undefined };
+  },
   computed: {
      url() { return '/api/comptes/' + this.id; }
   },
 
   methods: {
     set(v) {
-        axios.put(this.url, v).then(r => r.data).then(function (resp: any) {
+        axios.put(this.url, v).then(r => r.data).then((resp: any) => {
             if (resp && resp.success) {
                 this.finished = true;
             }
@@ -20,7 +22,7 @@ const Validate : vuejs.ComponentOption = {
     },
   },
   mounted() {
-    axios.get(this.url).then(r => r.data).then(function (sv: SVRaw) {
+    axios.get(this.url).then(r => r.data).then((sv: SVRaw) => {
         if (sv.error) {
             alert(sv);
         } else {
