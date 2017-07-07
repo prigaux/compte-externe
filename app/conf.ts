a3c159efa278dc13fe290c97f1ce0678d536d788
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-07-07 15:08:28 +0200

    cleanup: use modern syntax in app/conf.ts

--- app/conf.ts
+++ app/conf.ts
@@ -1,9 +1,9 @@
 'use strict';
 
-var wsgroupsURL = "https://wsgroups.univ-paris1.fr";
+const wsgroupsURL = "https://wsgroups.univ-paris1.fr";
 
-var conf = {
-    printCardUrl: function (login) { return "http://unicampus.univ.fr/unicampus/DesktopDefault.aspx?tabindex=1&tabid=2&NomCon=Xxxx&id_vis=" + login; },
+const conf = {
+    printCardUrl: (login) => `http://unicampus.univ.fr/unicampus/DesktopDefault.aspx?tabindex=1&tabid=2&NomCon=Xxxx&id_vis=${login}`,
     wsgroupsURL,
     
     attr_labels: {
@@ -40,14 +40,14 @@ var conf = {
         userPassword: 'Veuillez choisir un mot de passe comportant au moins 8 caractères. Ce mot de passe doit contenir des lettres minuscules, des lettres majuscules et des chiffres.',
         phone: 'Le champ doit être un numéro de téléphone. Exemple: 01 02 03 04 05',
         frenchPostalCode: 'Le champ doit être un code postal. Exemple: 75013',
-        forbiddenChars: function (forbiddenChars) {
-            return forbiddenChars.length == 1 ?
-             "Le caractère « " + forbiddenChars + " » n'est pas autorisé." :
-             "Les caractères « " + forbiddenChars + " » ne sont pas autorisés.";
-        },
-        noModerators: function (structureName) {
-            return "Le service " + structureName + " n'a pas de responsables déclarés dans le logiciel SIHAM. Il faut que les responsables en fassent la demande vers l'application de gestion de compte de l'ENT";
-        },
+        forbiddenChars: (forbiddenChars) => (
+            forbiddenChars.length == 1 ?
+             `Le caractère « ${forbiddenChars} » n'est pas autorisé.` :
+             `Les caractères « ${forbiddenChars} » ne sont pas autorisés.`
+        ),
+        noModerators: (structureName) => (
+            `Le service ${structureName} n'a pas de responsables déclarés dans le logiciel SIHAM. Il faut que les responsables en fassent la demande vers l'application de gestion de compte de l'ENT`
+        ),
     },        
 
     assets: {
