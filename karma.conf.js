52b054f7b1866dba604b7bc949a6862946deb404
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-07-18 20:38:45 +0200

    cleanup: configure karma not to need app/node_modules symlink

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,6 @@
 'use strict';
 var assets = require('./app/conf').assets;
-var files = assets.lib.js.concat(assets.js, assets.tests).map(function (f) { return "app/" + f });
+var files = assets.lib.js.concat(assets.js.concat(assets.tests).map(function (f) { return "app/" + f }));
 
 module.exports = function(config){
     config.set({
