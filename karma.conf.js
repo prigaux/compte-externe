5a2ac222cfa859202ec47c8eb505e00689e36f14
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-07-18 20:39:33 +0200

    dirty hack to fix karma tests (exclude axios.js)
    workaround for:
    
      TypeError: 'undefined' is not a function (evaluating 'window.btoa.bind(window)')
      at /home/marie/tmp/compte-externe/node_modules/axios/dist/axios.js:691

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,7 @@
 'use strict';
 var assets = require('./app/conf').assets;
 var files = assets.lib.js.concat(assets.js.concat(assets.tests).map(function (f) { return "app/" + f }));
+files = files.filter(f => !f.match(/axios/));
 
 module.exports = function(config){
     config.set({
