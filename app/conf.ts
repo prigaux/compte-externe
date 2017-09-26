ec541c4cbf0e31f481a322114f58ea8fcd70c93b
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2017-09-26 13:24:46 +0200

    authorize international phone numbers

--- app/conf.ts
+++ app/conf.ts
@@ -34,12 +34,15 @@ const conf = {
     title: "Gestion de comptes",
 
     pattern: {
-        phone: "(0|\\+33)(\\s*[0-9]\\s*){9}",
+        phone: [
+            "0(\\s*[0-9]){9}", // french
+            "\\+(1|2[07]|3[0-469]|4[013-9]|5[1-8]|6[0-6]|7|8[1246]|9[0-58]|[0-9]{3})(\\s*[0-9]){4,14}", // international country code, less than 15 digits
+        ].join('|'),
         frenchPostalCode: "\\s*[0-9]{5}\\s*",
     },
     error_msg: {
         userPassword: 'Veuillez choisir un mot de passe comportant au moins 8 caractères. Ce mot de passe doit contenir des lettres minuscules, des lettres majuscules et des chiffres.',
-        phone: 'Le champ doit être un numéro de téléphone. Exemple: 01 02 03 04 05',
+        phone: 'Le champ doit être un numéro de téléphone. Exemples : 01 02 03 04 05  ou  +41 66 555 44 33  ou  +886 1 1234 5678',
         frenchPostalCode: 'Le champ doit être un code postal. Exemple: 75013',
         forbiddenChars: (forbiddenChars) => (
             forbiddenChars.length == 1 ?
