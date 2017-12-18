770000ccd6668ecb2ed13fbddc012c0aaf502348
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2017-12-18 13:38:35 +0100

    modernize default steps/conf.ts

--- server/steps/conf.ts
+++ server/steps/conf.ts
@@ -1,90 +1,58 @@
 'use strict';
 
-import _ = require('lodash');
-import express = require('express');
-import actions = require('./actions');
-import acl = require('./acl');
-import main_conf = require('../conf');
+import * as _ from 'lodash';
+import * as actions from './actions';
+import * as acl from './acl';
+import * as utils from '../utils';
+import * as conf from '../conf';
 
-function readonly(attrs): StepAttrsOption {
-    return _.mapValues(attrs, options => (
-        _.defaults({ readonly: true }, options)
-    ));
-}
 
 const attrs: StepAttrsOption = {
     status: {},
     barcode: {},
-    supannCivilite: {},
+    supannCivilite: { choices: conf.ldap.people.supannCiviliteChoices },
     sn: {},
     givenName: {},
-    birthName: {},
+    birthName: { optional: true },
     birthDay: {},
-    homePostalAddress: {},
+    homePostalAddress: {},   
     homePhone: {},
     supannMailPerso: {},
     structureParrain: {},
 };
 
-const moderator_attrs = <StepAttrsOption> _.defaults({
+const moderator_attrs = _.defaults(<StepAttrsOption> {
     uid: {},
     supannAliasLogin: {},
-    structureParrain: { readonly: true },
 }, attrs);
 
-export const steps: steps = {
+const initialSteps: steps = {
     extern: {
-        attrs: attrs,
-        next: 'homonymes',
-    },
-
-    federation: {
-        attrs: attrs,
-        action_pre: actions.getShibAttrs,
-        next: 'homonymes',
-    },
-
-    cas: {
-        attrs: {},
-        action_post: actions.getCasAttrs,
+        labels: {
+            okButton: "Demander la création",
+        },      
+        attrs,
         next: 'moderate',
-    },
+    },       
+};
 
-    homonymes: {
-        acls: [acl.user_id('prigaux'),
-               acl.user_id('fchevreau'),
-               acl.user_id('branciar'),
-               //acl.ldapGroup("employees.administration.DGHB")
-              ],
-        notify: { added: 'homonymes_check.html',
-                  accepted: 'homonymes_done.html',
-                  rejected: 'moderation_rejected.html' },
-        attrs: moderator_attrs,
-        next: 'validate_email',
-    },
-
-    validate_email: {
-        action_pre: actions.sendValidationEmail,
-        attrs: readonly(moderator_attrs),
-        next: 'moderate',
-    },
-
+const nextSteps: steps = {
     moderate: {
-        acls: [acl.autoModerateIf(v => v.noInteraction),
-               acl.user_id('prigaux'),
-               acl.user_id('fchevreau'),
-               acl.user_id('branciar'),
-               //acl.ldapGroup("employees.administration.DGHB")
-              ],
-        notify: { added: 'moderation_needed.html',
-                  accepted: 'moderation_accepted.html',
-                  rejected: 'moderation_rejected.html' },
+        acls: [ acl.user_id('xxx') ],
+        labels: {
+            added: "La création de votre compte attend maintenant la modération. Vous serez prévenu par mail.",
+            title: "Modérer la création du compte",
+            okButton: "Approuver",
+            cancelButton: "Rejeter",
+        },
+        notify: {
+            added: 'moderation_needed.html',
+            accepted: 'moderation_accepted.html',
+            rejected: 'moderation_rejected.html',
+        },
         attrs: moderator_attrs,
         action_post: actions.createCompte
     },
 };
 
-// allow remapping. The initial steps mostly comes from welcome-create.html
-export function firstStep(step: string, req: express.Request): string {
-    return step;
-}
+export const steps = utils.mergeSteps(initialSteps, nextSteps);
