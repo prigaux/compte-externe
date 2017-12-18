75a3d273316424e05195ea15e58b22a34bc0a588
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2017-12-18 12:00:36 +0100

    cleanup: use "@types/concat-stream"

--- server/types.d.ts
+++ server/types.d.ts
@@ -55,11 +55,3 @@ declare module 'ldapjs' {
     export class NoSuchObjectError { constructor(any) }
 }
 
-declare module 'concat-stream' {
-  import { Writable } from 'stream';
-
-  function concatStream (cb: (data: any) => any): Writable
-  function concatStream (opts: { encoding: string }, cb: (data: any) => any): Writable
-
-  export = concatStream
-}
