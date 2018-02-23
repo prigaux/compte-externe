b92858ee66031564c8e5b52dac5618146ae713a0
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2018-02-23 12:33:08 +0100

    cleanup: attrsForm.ts does not need to handle validity anymore, it is done in <genericAttr>, <BarcodeAttrs>...

--- app/attrs/attrsForm.ts
+++ app/attrs/attrsForm.ts
@@ -1,5 +1,4 @@
 import Vue from "vue";
-import conf from '../conf';
 
 import genericAttr from './genericAttr.vue';
 import BarcodeAttrs from './BarcodeAttrs.vue';
@@ -11,25 +10,16 @@ export default Vue.extend({
     props: ['v', 'v_orig', 'attrs', 'step_labels'],
     data() {
         return {
-            validity: { submitted: false, supannCivilite: {}, givenName: {}, sn: {}, birthName: {}, homePhone: {}, telephoneNumber: {}, roomAccess: {}, floorNumber: {}, roomNumber: {}, supannMailPerso: {}, structureParrain: {}, duration_or_enddate: {}, duration: {}, enddate: {}, profilename: {} },
+            submitted: false,
         };
     },
     template,
     components: { genericAttr, BarcodeAttrs },
 
-    computed: {
-        label() {
-            return conf.attr_labels;
-        },
-        submitted() {
-            return this.validity.submitted;
-        },
-    },
-
     methods: {
       submit(event) {
           console.log("submit");
-          this.validity.submitted = true;
+          this.submitted = true;
           if (!event.target.checkValidity()) return Promise.resolve();
           
           return new Promise((resolve, reject) => {
