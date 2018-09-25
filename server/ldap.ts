af5ea36e6b80267dee26aca1e6e8b63fa4d94077
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2018-09-25 16:59:45 +0200

    convertAttrToLdap: ignore "profilename_to_modify"

--- server/ldap.ts
+++ server/ldap.ts
@@ -171,7 +171,7 @@ function convertAttrToLdap(attr: string, attrType: LdapAttrValue, conversion: ld
             return v.toString();
         } else if (attr === 'dn' || attr === 'objectClass') {
             return v.toString();
-        } else if (['noInteraction', 'various', 'duration_or_enddate', 'etablissementExterneOrNew', 'charter'].includes(attr)) {
+        } else if (['noInteraction', 'various', 'duration_or_enddate', 'etablissementExterneOrNew', 'charter', 'profilename_to_modify'].includes(attr)) {
             return '';
         } else {
             console.error(`unknown type for attribute ${attr}`);
