74271d097ac9e96c390a09491a6dc717fc17da72
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2019-02-06 09:46:07 +0100

    fixup! add actions esup_activ_bo_validateCode, esup_activ_bo_sendCode, esup_activ_bo_validateAccount

--- server/steps/actions_pre.ts
+++ server/steps/actions_pre.ts
@@ -47,10 +47,10 @@ export const getExistingUserWithProfile: simpleAction = (req, _sv)  => (
 );
 
 export const esup_activ_bo_validateAccount = (isActivation: boolean) : simpleAction => (req, _sv) => {
-    const userInfo = ldap.convertToLdap(conf.ldap.people.types, conf.ldap.people.attrs, search_ldap.v_from_WS(req.query), {});
+    const userInfo = ldap.convertToLdap(conf.ldap.people.types, conf.ldap.people.attrs, search_ldap.v_from_WS(req.query), { toJson: true });
     delete userInfo.code;
     const { wantedConvert, attrRemapRev } = ldap.convert_and_remap(conf.ldap.people.types, conf.ldap.people.attrs);
-    return esup_activ_bo.validateAccount(userInfo as any, Object.keys(attrRemapRev)).then(o => {
+    return esup_activ_bo.validateAccount(userInfo as any, _.without(Object.keys(attrRemapRev), 'userPassword')).then(o => {
         if (isActivation && !o.code) throw "Compte déjà activé";
         if (!isActivation && o.code) throw "Compte non activé";
         return { ..._.pick(o, 'possibleChannels', 'code'), ... ldap.handleAttrsRemapAndType(o as any, attrRemapRev, conf.ldap.people.types, wantedConvert) }
