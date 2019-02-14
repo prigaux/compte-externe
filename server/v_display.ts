00cae7831a36eb1102369dd92637c12bba8d9718
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2019-02-14 16:07:40 +0100

    v_display.various.diff: handle "attrs" specified in server/steps/conf.ts

--- server/v_display.ts
+++ server/v_display.ts
@@ -10,29 +10,29 @@ function key2name(raw, spec: StepAttrOption) {
     return raw;
 }
 
-const format_various_diff = (diff) => (
+const format_various_diff = (diff, attrs) => (
 `<table border="1">
   <tr><th>Champ modifié</th><th>Ancienne valeur</th><th>Nouvelle valeur</th></tr>
 ` +
     map(diff, ({ prev, current }, key) => {
-        const opts = client_conf.default_attrs_opts[key];
+        const opts = { ...client_conf.default_attrs_opts[key], ...attrs[key] };
         return '  <tr>' + [opts && opts.title || key, prev || '<i>aucune</i>', current || '<i>supprimée</i>'].map(s => `<td>${s}</td>`).join('') + '</tr>'
     }).join("\n") + `
 </table>`
 );
 
 const various_proxy = {
-    get(v, attr) {        
+    get({ various, attrs }, attr) {        
         return attr === 'diff' ?
-            format_various_diff(v[attr]) :
-            v[attr];
+            format_various_diff(various[attr], attrs) :
+            various[attr];
     }    
 }
 
 const v_proxy = {
     get({ v, attrs }, attr) {        
         return attr === 'various' ? 
-            new Proxy(v[attr] || {}, various_proxy) :
+            new Proxy({ various: v[attr] || {}, attrs }, various_proxy) :
             key2name(v[attr], attrs[attr]);
     }
 }
