730d1d5dd4214471ab81e1f99dc6ffc3ca511457
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2019-04-29 10:13:26 +0200

    ensure vue+ts+webpack do not prefer shared/conf.js over shared/conf.ts

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   runtimeCompiler: true,
   publicPath: "/",
+  // workaround for https://github.com/vuejs/vue-cli/issues/3898
+  chainWebpack: config => {
+    config.resolve.extensions.prepend('.ts')
+  },
 }
