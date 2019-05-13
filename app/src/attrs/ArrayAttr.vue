5cb7497f5acb22dbc2f68ddd947ad64c3a14b891
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2019-05-13 09:50:04 +0200

    <ArrayAttr>: handle readOnly

--- app/src/attrs/ArrayAttr.vue
+++ app/src/attrs/ArrayAttr.vue
@@ -5,10 +5,10 @@
     <div v-for="(item, i) in val">
         <genericAttr :name="name + (i ? '-' + i : '')" :opts="i ? item_opts : first_item_opts" :value="item" @input="v => set_item(i, v)" @remove="_ => remove_item(i)"
                 :stepName="stepName"
-                :allow_remove="opts.optional || i > 0" :submitted="submitted">
+                :allow_remove="!opts.readOnly && (opts.optional || i > 0)" :submitted="submitted">
         </genericAttr>
     </div>
-    <div :class="'col-sm-offset-' + (val.length ? 10 : 7)" class="col-sm-2" style="padding: 0 0 2rem 0">
+    <div :class="'col-sm-offset-' + (val.length ? 10 : 7)" class="col-sm-2" style="padding: 0 0 2rem 0" v-if="!opts.readOnly">
         <button class="btn btn-info" style="width: 100%" type="button" @click="val.push('')"><i class="glyphicon glyphicon-plus"></i></button>
     </div>
     <my-bootstrap-form-group>
