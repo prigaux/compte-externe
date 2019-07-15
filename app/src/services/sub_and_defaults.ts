72c4a4d2792f65a7285799dc6c963eee8680181c
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2019-07-15 20:03:10 +0200

    cleanup: compute "current_defaults" in the loop (needed for "properties")

--- app/src/services/sub_and_defaults.ts
+++ app/src/services/sub_and_defaults.ts
@@ -1,5 +1,5 @@
 import { V, StepAttrsOption, StepAttrOption, StepAttrOptionChoices } from '../services/ws';
-import { find, forIn, mapValues, pickBy, Dictionary } from 'lodash';
+import { find, forIn, Dictionary } from 'lodash';
 
 const find_choice = (oneOf: StepAttrOptionChoices[], val) => (
     find(oneOf, choice => choice.const === val)
@@ -28,18 +28,18 @@ const may_set_default_value = (k: string, opts: StepAttrOption, v, prev_defaults
 };
  
 export function compute_subAttrs_and_handle_default_values(attrs : StepAttrsOption, prev_defaults: Dictionary<string>, v: V) {
+    let current_defaults = {};
     const rec = (attrs : StepAttrsOption, attrs_: StepAttrsOption) => {
         forIn(attrs, (opts : StepAttrOption, k) => {
             attrs_[k] = { ...attrs_[k] || {}, ...opts }; // merge
             may_set_default_value(k, attrs_[k], v, prev_defaults || {});
+            if ("default" in opts) current_defaults[k] = opts.default;
 
             handle_chosen_oneOf_sub(opts, v[k], attrs => rec(attrs, attrs_));
         });
         return attrs_;
     }
     let attrs_ = rec(attrs, {});
-
-    let current_defaults = mapValues(pickBy(attrs_, (opts) => "default" in opts), (opts) => opts['default']);
 
     return { attrs: attrs_, current_defaults };
 }
