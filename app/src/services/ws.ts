bde74e022aea43431ea1b85fd6d972b7054a79bf
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2019-10-04 16:27:32 +0200

    cleanup: remove unused code in Ws.getInScope

--- app/src/services/ws.ts
+++ app/src/services/ws.ts
@@ -214,7 +214,6 @@ export const people_search = (step: string, token: string, maxRows? : number) : 
                         // pass v_orig to attrs opts.validator:
                         handleAttrsValidators(all_attrs, $scope.v_orig);
                     }
-                    sv.modifyTimestamp = new Date(sv.modifyTimestamp); // unused??
                     Helpers.eachObject(all_attrs, (attr, opts) => {
                         const default_ = fromWs_one(attr, params[`default_${attr}`] || hash_params[`default_${attr}`], all_attrs);
                         const set_ = fromWs_one(attr, opts.uiType !== 'newPassword' && params[attr] || params[`set_${attr}`] || hash_params[`set_${attr}`], all_attrs);
