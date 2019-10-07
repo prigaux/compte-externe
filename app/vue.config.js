e5293b82b2a3c8e95a93ca6a787ce06e3e1af2c2
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2019-10-07 13:28:40 +0200

    recent @vue/cli-service needs a workaround
    Cf https://github.com/vuejs/vue-cli/issues/4557#issuecomment-532805642

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,8 +1,10 @@
 module.exports = {
   runtimeCompiler: true,
   publicPath: "/",
-  // workaround for https://github.com/vuejs/vue-cli/issues/3898
   chainWebpack: config => {
+    // workaround for https://github.com/vuejs/vue-cli/issues/3898
     config.resolve.extensions.prepend('.ts')
+    // workaround for https://github.com/vuejs/vue-cli/issues/4557#issuecomment-532805642
+    config.plugins.delete('progress')
   },
 }
