2979106c2e02cbf0ef0837c6b3252f39b5c51616
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-01-02 12:02:09 +0100

    use HTML5 autocomplete=new-password when prompting for new password

--- app/src/attrs/PasswordAttr.vue
+++ app/src/attrs/PasswordAttr.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
   <my-bootstrap-form-group name="userPassword" :label="opts.title" :validity="validity" hideErrors=1>
-    <input-with-validity name="userPassword" v-model="val" type="password" :pattern="passwordPattern" required :validity.sync="validity.userPassword"></input-with-validity>
+    <input-with-validity name="userPassword" v-model="val" type="password" autocomplete="new-password" :pattern="passwordPattern" required :validity.sync="validity.userPassword"></input-with-validity>
     <span class="help-block" v-if="!validity.userPassword.valid">{{error_msg.userPassword}}</span>
   </my-bootstrap-form-group>
 
   <my-bootstrap-form-group name="userPassword2" :label="default_attrs_title.userPassword2" :validity="validity" hideErrors=1>
-    <input-with-validity name="userPassword2" v-model="userPassword2" type="password" :same-as="val" required :validity.sync="validity.userPassword2"></input-with-validity>
+    <input-with-validity name="userPassword2" v-model="userPassword2" type="password" autocomplete="new-password" :same-as="val" required :validity.sync="validity.userPassword2"></input-with-validity>
     <span class="help-block" v-if="validity.userPassword2.patternMismatch && !validity.userPassword.patternMismatch">Les mots de passe ne sont pas identiques</span>
   </my-bootstrap-form-group>
 </div>  
