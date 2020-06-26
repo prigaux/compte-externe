d2e567c138245bab2a3cd8e44362f9130d226adb
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2020-06-26 14:09:09 +0200

    v_display: fix date conversion to string DD/MM/YYYY
    https://projets.univ-paris1.fr/project/5/task/1904

--- server/v_display.ts
+++ server/v_display.ts
@@ -13,8 +13,11 @@ export const to_ul_li = (vals: string[]) => (
     `, { vals }) : ''
 )
 
+const two_digit = (n: number | string) => ("" + n).padStart(2, "0")
 const to_DD_MM_YYYY = (date: Date) => (
-    date.toLocaleDateString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit' })
+    //date.toLocaleDateString('en-GB', { year: 'numeric', month: '2-digit', day: '2-digit' })
+    // do it by hand until we force nodejs >= 13 which has full-icu by default 
+    [ date.getDate(), date.getMonth() + 1, date.getFullYear() ].map(two_digit).join('/')
 )
 
 async function key2name(raw, spec: StepAttrOption, if_empty : string = '') {
