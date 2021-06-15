a6abfe7172945ba6306e09b91361a656dd173b9d
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2021-06-15 17:47:47 +0200

    workaround "Error: error while parsing tsconfig.json"
    this bug occurs when ts-loader have to compile shared/*.ts and not finding any tsconfig.json

--- app/vue.config.js
+++ app/vue.config.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 module.exports = {
   runtimeCompiler: true,
   publicPath: "/",
@@ -5,4 +7,11 @@ module.exports = {
     // workaround for https://github.com/vuejs/vue-cli/issues/4557#issuecomment-596272497
     progress: false,
   },
+  // workaround "Error: error while parsing tsconfig.json"
+  // (occurring when ts-loader have to compile shared/*.ts and not finding any tsconfig.json)
+  chainWebpack: config => {
+    config.module.rule('ts').use('ts-loader').tap(options => (
+        { ...options, configFile: path.resolve(__dirname, 'tsconfig.json') }
+    ))
+  }
 }
